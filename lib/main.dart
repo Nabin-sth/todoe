564d70c6be6ddfb825a57d12e941731d80c2237c
Nabin-sth <nabinshrestha120@gmail.com>
2024-02-06 21:29:31 +0545

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: TasksScreen(callback: () {
-        print("object");
+        print("neww");
       }),
       debugShowCheckedModeBanner: false,
     );
